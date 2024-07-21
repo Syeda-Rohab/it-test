143554d1abc6bd7ac4cc07d914cb511570d17125
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-21 07:18:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+//1) let message = "Rohab";
+//   console.log(message);
+//  let num1 = 15;
+//  let num2 = 15;
+//  console.log(num1 + num2);
+//  console.log(num1 - num2);
+//  console.log(num1 * num2);
+//  console.log(num1 / num2);
+//3)  let age = 16;
+//
+//if (age < 18) {
+//console.log("Adult")
+//} else {
+//console.log("Minor")
+//}
+//for(let i = 1; i <= 5; i++) {
+// console.log(i)
+//}
+//for (let i = 1; i <= 10 ; i++ ) {
+// console.log(i)
+//}
+//function greet (name:string) {
+//console.log(`hello ${name}`)
+//}
+//greet ("owais")  
+function printDetails(name, age) {
+    console.log("name:[name]");
+}
+console.log("age:[age]");
